ec59864ec687d638d6f6c49d231941156f8cf776
Francisco Ribeiro <asd@asd.com>
2017-05-03 13:02:25 +0100

    continuation of previous commit

---
+++ module_name/module_name.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Project Name
+# Francisco Ribeiro <francisco@ironik.org>
+####################################################################################################
--- module_name/settings.py
+++ module_name/settings.py
@@ -5,10 +5,11 @@
 ####################################################################################################
 import os, logging
 
-LOGGING_VERBOSITY = logging.DEBUG
-PROG_NAME = os.path.basename(os.path.dirname(__file__))
-
 # folder locations
 project_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
 dataset_folder = os.path.abspath(os.path.join(project_folder, 'data'))
 logs_folder = os.path.abspath(os.path.join(project_folder, 'logs'))
+
+LOGGING_VERBOSITY = logging.DEBUG
+PROG_NAME = os.path.basename(os.path.dirname(project_folder))
+
